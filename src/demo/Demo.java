a8eeb1086e4857abfe1954332d8ef63438a585e2
David Warzynski <David Warzynski@DESKTOP-IP9BH6C>
2022-10-05 00:27:45 +0200

    cambio 3 solucionado

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,11 +3,7 @@ package demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-<<<<<<< HEAD
-		System.out.println("cambio 3 mars en conflicto");
-=======
-		System.out.println("cambio 3 luna");
->>>>>>> refs/heads/master
+		System.out.println("cambio 3 SOLUCIONADO");
 		System.out.println("linea 3");
 		/*cambio desde mars*/
 	}
